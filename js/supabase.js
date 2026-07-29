d21a323919195342173fc4770d3291b901cd7b4d
Youssef7shop <tangawirp40@gmail.com>
2026-07-29 05:05:07 +0100

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-// js/supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
